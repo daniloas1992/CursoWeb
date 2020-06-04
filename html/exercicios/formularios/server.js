3b5a9af4bdbbc776d8aa9c3ddabca660e59f8c08
Danilo <danilo_as1992@hotmail.com>
2020-06-03 23:47:47 -0300

    HTML listas,links, tabelas e formulários

---
+++ html/exercicios/formularios/server.js
@@ -0,0 +1,14 @@
+// Rodar no terminal: node server.js
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3003)
