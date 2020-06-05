ae42a5a0804ba356efade549c9968bd9593718ce
Danilo <danilo_as1992@hotmail.com>
2020-06-04 23:35:59 -0300

    Formulários final e Imagens

--- html/exercicios/formularios/server.js
+++ html/exercicios/formularios/server.js
@@ -1,4 +1,5 @@
-// Rodar no terminal: node server.js
+// Rodar no terminal sem nodemon: node server.js
+// Rodar no terminal com nodemon: nodemon server.js
 
 const express = require('express')
 const app = express()
@@ -8,7 +9,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Usuário Incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado!</h1>')
 })
 
 app.listen(3003)
